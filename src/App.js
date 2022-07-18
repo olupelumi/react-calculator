ee41296e496b0112aab857e5e628d33c5f8d1b14
Oluwapelumi Fafowora <oluwapelumifafowora@Oluwapelumis-MacBook-Pro.local>
2022-07-17 17:44:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="calculator-grid">
+    <div className="output">
+      {/* In 5 + 6 = 11, 5 and 6 are both operands */}
+      <div className = "previous-operand">
+        667*447
+      </div>
+      <div className = "current-operand"> 
+      {/* Number we're in the act of typing out */}
+      </div>
     </div>
+    <button className="span-two">
+        {/* classname is "span-two" because this will span two columns */}
+         AC 
+         </button>
+      <button> DEL </button>
+      <button> ÷ </button>
+      <button> 1 </button>
+      <button> 2 </button>
+      <button> 3 </button>
+      <button> * </button>
+      <button> 4 </button>
+      <button> 5 </button>
+      <button> 6 </button>
+      <button> + </button>
+      <button> 7 </button>
+      <button> 8 </button>
+      <button> 9 </button>
+      <button> - </button>
+      <button> . </button>
+      <button> 0 </button>
+      <button className="span-two"> = </button>
+
+
+   </div>
   );
 }
 
