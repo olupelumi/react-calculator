44d9bad8cedc8a8ac7f408d58c404f225f2271b9
Oluwapelumi Fafowora <oluwapelumifafowora@Oluwapelumis-MacBook-Pro.local>
2022-07-20 13:44:51 -0700

    add 1 as a digit button

--- src/App.js
+++ src/App.js
@@ -48,22 +48,20 @@ function App() {
          </button>
       <button> DEL </button>
       <button> ÷ </button>
-      <button 
-      onClick={() => dispatch({type: "insert1", payload: "1"})}
-      > 1 </button>
-      <DigitButton digit={2} dispatch={dispatch}/>
-      <DigitButton digit={3} dispatch={dispatch}/>
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
       <button> * </button>
-      <DigitButton digit={4} dispatch={dispatch}/>
-      <DigitButton digit={5} dispatch={dispatch}/>
-      <DigitButton digit={6} dispatch={dispatch}/>
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
       <button> + </button>
-      <DigitButton digit={7} dispatch={dispatch}/>
-      <DigitButton digit={8} dispatch={dispatch}/>
-      <DigitButton digit={9} dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
       <button> - </button>
-      <button> . </button>
-      <DigitButton digit={0} dispatch={dispatch}/>
+      <DigitButton digit="." dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
       <button className="span-two"> = </button>
    </div>
   );
